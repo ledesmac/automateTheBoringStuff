fdf0b6736252e739d2a2f5835bca3c8c16eabe83
Alex Ledesma <alex.ledesma7@gmail.com>
2023-04-17 17:32:59 -0400

    started work on problem 2 of chapter 9, need to figure out how to handle edge cases

--- 9-rwFiles/MadLibs/madlibs.py
+++ 9-rwFiles/MadLibs/madlibs.py
@@ -3,9 +3,20 @@
 
 import sys
 import argparse
+import re
 
 # Create argument parser to take in arguments from command line
 parser = argparse.ArgumentParser(description="interactive python program to allow user to create madlib files")
-parser.add_argument("-f", "--file", help="File to load for MadLib script")
+parser.add_argument("file", help="File to load for MadLib script")
 
 args = parser.parse_args()
+
+if args.file:
+    madLib = open(args.file, 'r')
+    words = re.split(' |\n', madLib.read())
+    #doesnt work for specific cases, such as 'VERB.', need to figure out how to work with that
+    for word in words:
+        if word == 'ADJECTIVE' or word == 'NOUN' or word == 'VERB':
+            replace = input(f'Enter a {word.lower()}:')
+
+    print(words)
